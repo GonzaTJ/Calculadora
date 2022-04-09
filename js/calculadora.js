f712d196a3ecd9b59f3452f737e77219e0476e95
GonzaTJ <95258624+GonzaTJ@users.noreply.github.com>
2022-04-09 18:50:56 -0300

    Update calculadora.js

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,18 +1,55 @@
-window.addEventListener('load', ()=>{ /* Escuchamos cuando se carga el documento */
+window.addEventListener('load', () => {
+    /* Busco los botones y el display */
+    const keypadButtons = document.getElementsByClassName('keypad-button'); /* no usar querySelector porque devuelve un NodeList en vez de un HTML Collection */
+    const display = document.querySelector('.calculator-display'); /* usar querySelector para que nos de el elemento solo */
     
-    /* Creamos dos constantes y nos guardamos los elementos que necesitamos */
-    const display = document.querySelector('calculator-display');
-    const keypadButtons = document.getElementsByClassName('keypad-button');
-
+    /* Convierto la HTMLCollection a Array para poder iterar */
     const keypadButtonsArray = Array.from(keypadButtons);
-    /* Creamos otra constante para convertir el HTMLCollection a Array */
-    keypadButtonsArray.forEach ( (button)=>{
+    
+    /* Agrego un evento de click a cada boton */
+    keypadButtonsArray.forEach( (button) => {
 
-        /* A cada boton le agregamos un listener */
-        button.addEventListener('click', ()=>{
-            console.log(button.innerHTML);
-        })
-    })
+        button.addEventListener('click', () => {
+            /* Llamo a una funcion cada vez que ocurra un click */
+            calculadora(button, display);
+        });
 
+    });
 });
 
+function calculadora(button, display) {
+    /* funcion que decide que hacer */
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+
+        case '=':
+            calcular(display);
+            break;
+    
+        default:
+            actualizar(display, button);
+            break;
+    }
+
+}
+
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML);
+}
+
+function actualizar(display, button) {
+    
+    if(display.innerHTML == 0){
+        display.innerHTML = '';
+    }
+
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display) {
+    if(display.innerHTML != 0) {
+        display.innerHTML = 0;
+    }
+}
